8e71b0fa00998633cef1d30ab4a207c6443053a2
Ariel <ariel040132@gmail.com>
2023-03-24 23:12:02 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
+const mongoose = require("mongoose");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+//import express
 const express = require("express");
+// const exphbs = require("express-handlebars");
 const app = express();
-const exphbs = require("express-handlebars");
-const port = 3000;
 
-app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-app.get("/", (req, res) => {
-  res.render("index");
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.listen(port, () => {
-  console.log(`Express app listening on port ${port}.`);
+//資料庫
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("mongobd error!");
+});
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
+
+// //路由
+// app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(3000, () => {
+  console.log("Express app listening on http://localhost:3000");
 });
