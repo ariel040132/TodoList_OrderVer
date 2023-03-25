dd6eac2ffef5bf5a386fc20154e9d8d15529d8aa
Ariel <ariel040132@gmail.com>
2023-03-25 15:27:01 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const exphbs = require("express-handlebars");
 const app = express();
-
+const Todo = require("./models/todo");
+console.log(Todo);
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,7 +27,10 @@ db.once("open", () => {
 });
 
 app.get("/", (req, res) => {
-  res.render("index");
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then((todos) => res.render("index", { todos: todos })) // 將資料傳給 index 樣板
+    .catch((error) => console.error(error)); //錯誤處理
 });
 
 app.listen(3000, () => {
